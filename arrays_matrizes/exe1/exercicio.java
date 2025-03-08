f5e5025dc2da32e1996634ae7dfa69016c1fd6cc
OttozaoJooj <otto.mozale007@gmail.com>
2025-03-08 12:24:46 -0300

    add: array matrizes folder

---
+++ arrays_matrizes/exe1/exercicio.java
@@ -0,0 +1,10 @@
+package arrays_matrizes.exe1;
+/*Ler um conjunto de n ´umeros reais, armazenando-o em vetor e calcular o quadrado das
+componentes deste vetor, armazenando o resultado em outro vetor. Os conjuntos t ˆem
+10 elementos cada. Imprimir todos os conjuntos. */
+
+public class exercicio {
+    public static void main(String[] args){
+        System.out.println("Saas");
+    }
+}
