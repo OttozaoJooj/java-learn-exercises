7e71f7e8476c5376aef217945ca18f56f27dd70a
OttozaoJooj <otto.mozale007@gmail.com>
2025-03-12 17:03:29 -0300

    modify: exe2 files from arrays and matrizes folder

--- arrays_matrizes/exe2/exercicio.java
+++ arrays_matrizes/exe2/exercicio.java
@@ -1,7 +1,41 @@
 package arrays_matrizes.exe2;
+import java.util.ArrayList;
+import java.util.Scanner;
+/*Faça um programa que leia um vetor de 8 posições e, em seguida, leia também dois va-
+lores X e Y quaisquer correspondentes a duas posições no vetor. Ao final seu programa
+deverá escrever a soma dos valores encontrados nas respectivas posições X e Y . */
+
+/*OBS:
+ * Para Preencher um array, basta usar, quando declara, Collections.nCopies(vezes, elemento);
+ * 
+ */
 
 public class exercicio {
     public static void main(String[] args){
-        System.out.println("hello wolrd");
+        Scanner iptScanner = new Scanner(System.in);
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        
+        int numbersLen = 8;
+
+        for(int i = 0; i < numbersLen; i++){
+
+            System.out.print("Digite um número:");
+            numbers.add(iptScanner.nextInt());
+        }
+
+        
+        System.out.print("Selecione um número: ");
+        int firstIndexSelected = iptScanner.nextInt() - 1;
+
+        System.out.print("Selecione mais um número: ");
+        int secondIndexSelected = iptScanner.nextInt() - 1;
+
+        iptScanner.close();
+
+        int sumNumbersSelected = numbers.get(firstIndexSelected) + numbers.get(secondIndexSelected);
+
+        System.out.println(String.format("A soma dos números selecionados dá %d", sumNumbersSelected));
+
     }
 }
