b01b790e9178d9a4a86b9890d33fb1a5a8fb1057
OttozaoJooj <otto.mozale007@gmail.com>
2025-03-12 15:44:00 -0300

    modify: exe2 files from logica_programacao folder

--- logica_programacao/exe2/exercicio.java
+++ logica_programacao/exe2/exercicio.java
@@ -1,4 +1,5 @@
 package exe2;
+
 /*Imprima a soma de 1 até 1000.*/
 public class exercicio {
     public static void main(String[] args){
@@ -9,5 +10,10 @@ public class exercicio {
             sum += i;
         }
         System.out.println("Soma do Intervalo de 1 a 1000 é " + sum);
+
+
+
+
+
     }
 }
