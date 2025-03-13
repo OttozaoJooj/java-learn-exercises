7a7388ad98a86cd3f6b96399748445546d5147a1
OttozaoJooj <otto.mozale007@gmail.com>
2025-03-13 17:33:31 -0300

    modify: exe4 files from array and matrizes folder

--- arrays_matrizes/exe4/exercicio.java
+++ arrays_matrizes/exe4/exercicio.java
@@ -1,7 +1,48 @@
 package arrays_matrizes.exe4;
+/*Faça um programa que receba do usuário um vetor com 10 posições. Em seguida deverá
+ser impresso o maior e o menor elemento do vetor. */
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class exercicio {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        ArrayList<Integer> numbers = new ArrayList<Integer>(10);
+        Scanner scannerIn = new Scanner(System.in);
+
+        int numbersLen = 10;
+        int maxNumber = 0;
+        int minNumber = 0;
+
+        for(int i = 0; i < numbersLen; i++){
+            System.out.println("Digite um número: ");
+            numbers.add(scannerIn.nextInt());
+                    
+        }
+
+        scannerIn.close();
+
+        for(int i = 0; i < numbersLen; i++){
+            if(i == 0){
+                maxNumber = numbers.get(0);
+                minNumber = numbers.get(0);
+
+            } else if(numbers.get(i) > maxNumber){
+                maxNumber = numbers.get(i);
+
+            } else if(numbers.get(i) < minNumber){
+                minNumber = numbers.get(i);
+            } else{
+                continue;
+            }
+        }
+
+        System.out.println(String.format("Maior número: %d", maxNumber));
+        System.out.println(String.format("Menor número: %d", minNumber));
+
+
+
+
+        
     }
 }
