12e71724199bfc350507c7604eb6f539a1287896
OttozaoJooj <otto.mozale007@gmail.com>
2025-03-15 16:03:15 -0300

    add: exe5 files from array and matrizes folder

---
+++ arrays_matrizes/exe5/exercicio.java
@@ -0,0 +1,43 @@
+package arrays_matrizes.exe5;
+/*Escreva um programa que leia 10 números inteiros e os armazene em um vetor. Imprima
+o vetor, o maior elemento e a posição que ele se encontra. */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class exercicio {
+    public static void main(String args[]){
+        ArrayList<Integer> numbers = new ArrayList<Integer>(10);
+        Scanner scannerIn = new Scanner(System.in);
+
+        int numbersLen = 10;
+        int maxNumber = 0;
+
+
+        for(int i = 0; i < numbersLen; i++){
+            System.out.print("Digite um número: ");
+            numbers.add(scannerIn.nextInt());
+        }
+
+        scannerIn.close();
+
+        maxNumber = numbers.get(0);
+
+        for (Integer number : numbers) {
+            if(number > maxNumber){
+                maxNumber = number;
+            }
+
+        }
+
+        System.out.println(String.format("O maior número é %d e está na posição %d", maxNumber, numbers.indexOf(maxNumber) + 1));
+
+    
+
+        
+
+        
+    }
+    
+
+}
