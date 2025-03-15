de73a354ebcc6a1b809e970ba141b87b4aaa783c
OttozaoJooj <otto.mozale007@gmail.com>
2025-03-15 16:03:36 -0300

    add: exe7 files from array and matrizes folder

---
+++ arrays_matrizes/exe7/exercicio.java
@@ -0,0 +1,35 @@
+package arrays_matrizes.exe7;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*Crie um programa que lê 6 valores inteiros pares e, em seguida, mostre na tela os valores
+lidos na ordem inversa. */
+
+public class exercicio {
+     public static void main(String args[]){
+        ArrayList<Integer> numbers = new ArrayList<Integer>(6);
+        Scanner scannerIn = new Scanner(System.in);
+
+        final int numbersLen = 6;
+        int numberIn;
+
+        for(int i = 0; i < numbersLen; i++){
+            do{
+                System.out.print(String.format("Digite o %dº número: ", i + 1));
+                numberIn = scannerIn.nextInt();
+
+            } while(numberIn % 2 != 0);
+
+            numbers.add(numberIn);
+        }
+
+        scannerIn.close();
+
+        for(int i = numbersLen - 1; i >= 0; i--){
+            System.out.println(numbers.get(i));
+        }
+
+
+    }
+}
